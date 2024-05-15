8f9185936e4c0f32f4fcfef14cfb378665533293
Tobias Vonier <tobias.vonier@students.uni-freiburg.de>
2024-05-15 14:10:36 +0200

    changed button ordering #20

--- pages/00_Veranstaltungen_edit.py
+++ pages/00_Veranstaltungen_edit.py
@@ -33,20 +33,11 @@ if st.session_state.logged_in:
     x = util.veranstaltung.find_one({"_id": st.session_state.edit})
     st.subheader(tools.repr(collection, x["_id"]))
     col1, col2, col3 = st.columns([1, 1, 1])
+
     with col1:
-        with st.popover('Veranstaltung löschen'):
-            s = ("  \n".join(tools.find_dependent_items(collection, x["_id"])))
-            if s:
-                st.write("Eintrag wirklich löschen?  \n" + s + "  \nwerden dadurch geändert.")
-            else:
-                st.write("Eintrag wirklich löschen?  \nEs gibt keine abhängigen Items.")
-            colu1, colu2, colu3 = st.columns([1,1,1])
-            with colu1:
-                submit = st.button(label = "Ja", type = 'primary', key = f"delete-{x['_id']}")
-            if submit:
-                tools.delete_item_update_dependent_items(collection, x["_id"])
-            with colu3: 
-                st.button(label="Nein", on_click = st.success, args=("Nicht gelöscht!",), key = f"not-deleted-{x['_id']}")
+        if st.button("Zurück ohne Speichern"):
+            switch_page("Veranstaltungen")
+
     with col2:
         with st.popover('Veranstaltung kopieren'):
             st.write("Kopiere " + tools.repr(collection, x["_id"]))
@@ -72,11 +63,21 @@ if st.session_state.logged_in:
                     st.rerun()
             with colu2: 
                 st.button(label="Abbrechen", on_click = st.success, args=("Nicht kopiert!",), key = f"not-copied-{x['_id']}")
-    with col3:
-        st.markdown('<span id="align-right"><\span>', unsafe_allow_html=True)
 
-        if st.button("Zurück ohne Speichern"):
-            switch_page("Veranstaltungen")
+    with col3:
+        with st.popover('Veranstaltung löschen'):
+            s = ("  \n".join(tools.find_dependent_items(collection, x["_id"])))
+            if s:
+                st.write("Eintrag wirklich löschen?  \n" + s + "  \nwerden dadurch geändert.")
+            else:
+                st.write("Eintrag wirklich löschen?  \nEs gibt keine abhängigen Items.")
+            colu1, colu2, colu3 = st.columns([1,1,1])
+            with colu1:
+                submit = st.button(label = "Ja", type = 'primary', key = f"delete-{x['_id']}")
+            if submit:
+                tools.delete_item_update_dependent_items(collection, x["_id"])
+            with colu3: 
+                st.button(label="Nein", on_click = st.success, args=("Nicht gelöscht!",), key = f"not-deleted-{x['_id']}")
 
     with st.expander("Grunddaten", expanded = True if st.session_state.expanded == "grunddaten" else False):
         with st.form(f'Grunddaten-{x["_id"]}'):
