958f1b42aec0b8ff3e4496cb47497b5180d7e1ee
pfaffelh <p.p@stochastik.uni-freiburg.de>
2024-10-23 11:08:43 +0200

    added ldap names

--- mongo/change20241023.py
+++ mongo/change20241023.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import pymongo
 import os
 import datetime
-
+import json
 cluster = MongoClient("mongodb://127.0.0.1:27017")
 mongo_db = cluster["vvz"]
 
@@ -18,6 +18,13 @@ mongo_db.command('collMod','person', validator=schema20241023.person_validator, 
 print("Ab hier wird verändert")
 
 per.update_many({}, { "$set" : { "url" : ""}})
+
+with open('ldap.json', 'r') as json_file:
+    data = json.load(json_file)
+
+for item in data:
+    print(item)
+    per.update_one({"vorname" : item["givenName"], "name" : item["sn"]}, {"$set" : { "url" : item["labeledURI"] if item["labeledURI"] is not None else "", "email" : item["mail"] if item["mail"] is not None else ""}})
 
 # Ab hier wird das Schema gecheckt
 print("Check schema")
